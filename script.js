8fd23c56323899f91d4f297a8423ae7f49ea7e1f
gcasq <gustavoacasqueiro@gmail.com>
2020-11-14 16:35:52 -0300

    Requisito 4: Gera cores aleatórias e insere nos círculos

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("oi")
+const balls = document.getElementsByClassName('ball');
+//commentary
+//elementos : tudo o que está entre tags, tipo 'div', 'p'
+//console.log(balls);
+//rgb: 0 - 255
+function numeroAleatorio(){
+    const numero =Math.floor(Math.random()*256); //retorna num aleatorio entre 0 e 255
+    return numero;    
+};
+
+function corAleatoria(){
+    const rgb = `rgb(${numeroAleatorio()}, ${numeroAleatorio()}, ${numeroAleatorio()})`;//transforma em texto!
+    return rgb;
+}
+
+for(let bola=0;bola<balls.length;bola++){
+    balls[bola].style.backgroundColor = corAleatoria();
+};
+
+console.log(corAleatoria());
